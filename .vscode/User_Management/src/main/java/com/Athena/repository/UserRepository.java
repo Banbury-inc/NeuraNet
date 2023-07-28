85778bb2bbd7c206c978fb3ea7fa7b7dfd914c44
Michael <mmills6060@gmail.com>
2023-07-28 11:21:51 -0400

    user management gui

---
+++ .vscode/User_Management/src/main/java/com/Athena/repository/UserRepository.java
@@ -0,0 +1,5 @@
+package main.java.com.Athena.repository;
+
+public class UserRepository {
+    
+}
---
+++ .vscode/User_Management/src/webapp/js/app.js
@@ -0,0 +1,14 @@
+document.getElementById("signupForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Handle user registration form submission using AJAX
+});
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    // Handle user login form submission using AJAX
+});
+
+// Function to fetch and display user profile information
+function showUserProfile() {
+    // Use AJAX to fetch user profile data and display it in the profileInfo div
+}
