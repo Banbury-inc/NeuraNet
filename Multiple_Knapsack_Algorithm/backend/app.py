46f863568322a4663176e4cb2391d72b588f965f
Michael <mmills6060@gmail.com>
2023-09-02 12:27:51 -0400

    working updating files and profile

--- Multiple_Knapsack_Algorithm/backend/app.py
+++ Multiple_Knapsack_Algorithm/backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from api import user_management
 from flask_cors import CORS
+from api import files
 
 app = Flask(__name__)
 CORS(app)
@@ -78,6 +79,50 @@ def get_from_ipfs():
     
     return jsonify(user_data)
 
- 
+'''
+Get all files that are owned by a user
+Enpoint:/get_files
+Method: GET
+Response Format: JSON
+
+Request:
+'cid' (string, required): The file's CID
+
+Response:
+Status: 200 OK
+Example Response:
+{
+  "first_name": "Michael",
+  "last_name": "Mills",
+  "userID": "d42e82de-f5de-4dfc-be94-d9872f0385d0",
+  "username": "john_doe"
+}
+'''
+
+# Sample data (in a real application, this data might come from a database)
+files = [
+    {"id": 1, "name": 'File 1', "size": 50, "priority": 3},
+    {"id": 2, "name": 'File 2', "size": 30, "priority": 2},
+    # Add more files here
+]
+
+@app.route("/get_files")
+def get_files():
+
+    return jsonify(files)
+
+@app.route("/get_file_info")
+def get_files():
+    cid = request.args.get('cid')
+
+    if not cid:
+        return jsonify({"message": "Missing cid"}), 400
+
+    user_files = user_management.get_from_ipfs(cid)
+    
+
+    return jsonify(files)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
