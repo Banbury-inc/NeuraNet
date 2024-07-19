1a601928118a7ec05f829821354c3115667ce897
Michael Mills <mmills6060@gmail.com>
2024-07-18 20:38:51 -0400

    got main2 working on my own pc

---
+++ Artificial_Intelligence/large_language_model/main2.py
@@ -0,0 +1,32 @@
+import transformers
+import torch
+
+model_id = "meta-llama/Meta-Llama-3-8B-instruct"
+
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto",
+)
+
+messages = [
+    {"role": "system", "content": "You are a pirate chatbot who always responds in pirate speak!"},
+    {"role": "user", "content": "Who are you?"},
+]
+
+terminators = [
+    pipeline.tokenizer.eos_token_id,
+    pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")
+]
+
+outputs = pipeline(
+    messages,
+    max_new_tokens=256,
+    eos_token_id=terminators,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
+)
+print(outputs[0]["generated_text"][-1])
+
